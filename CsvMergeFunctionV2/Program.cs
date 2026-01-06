9cf436d47fe62a7c1679f834f5cb5619d12c9c32
Erick Gaither <erick.gaither@gmail.com>
2026-01-06 09:02:02 -0500

    Register blob service client

--- CsvMergeFunctionV2/Program.cs
+++ CsvMergeFunctionV2/Program.cs
@@ -1,9 +1,23 @@
+using Azure.Storage.Blobs;
 using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var builder = FunctionsApplication.CreateBuilder(args);
 
 builder.ConfigureFunctionsWebApplication();
+
+var blobConnectionString = builder.Configuration.GetConnectionString("BlobStorage")
+    ?? builder.Configuration["AzureWebJobsStorage"]
+    ?? builder.Configuration["BlobStorageConnectionString"];
+
+if (string.IsNullOrWhiteSpace(blobConnectionString))
+{
+    throw new InvalidOperationException(
+        "Blob storage connection string not configured. Set AzureWebJobsStorage, BlobStorageConnectionString, or ConnectionStrings:BlobStorage.");
+}
+
+builder.Services.AddSingleton(new BlobServiceClient(blobConnectionString));
 
 // Application Insights isn't enabled by default. See https://aka.ms/AAt8mw4.
 // builder.Services
