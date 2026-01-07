df58bc1871f054ba41d6af08468b621db6288fe9
Erick Gaither <erick.gaither@gmail.com>
2026-01-07 08:53:24 -0500

    Register blob storage dependencies

--- CsvMergeFunctionV2/Program.cs
+++ CsvMergeFunctionV2/Program.cs
@@ -1,23 +1,36 @@
 using Azure.Storage.Blobs;
 using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 
 var builder = FunctionsApplication.CreateBuilder(args);
 
 builder.ConfigureFunctionsWebApplication();
 
-var blobConnectionString = builder.Configuration.GetConnectionString("BlobStorage")
-    ?? builder.Configuration["AzureWebJobsStorage"]
-    ?? builder.Configuration["BlobStorageConnectionString"];
+builder.Services.AddOptions<BlobStorageOptions>()
+    .Bind(builder.Configuration.GetSection("BlobStorage"))
+    .Validate(options => !string.IsNullOrWhiteSpace(options.ConnectionString), "BlobStorage:ConnectionString is required.")
+    .ValidateOnStart();
 
-if (string.IsNullOrWhiteSpace(blobConnectionString))
+builder.Services.AddSingleton(sp =>
 {
-    throw new InvalidOperationException(
-        "Blob storage connection string not configured. Set AzureWebJobsStorage, BlobStorageConnectionString, or ConnectionStrings:BlobStorage.");
-}
+    var configuration = sp.GetRequiredService<IConfiguration>();
+    var options = sp.GetRequiredService<IOptions<BlobStorageOptions>>().Value;
+    var blobConnectionString = options.ConnectionString
+        ?? configuration.GetConnectionString("BlobStorage")
+        ?? configuration["AzureWebJobsStorage"]
+        ?? configuration["BlobStorageConnectionString"];
 
-builder.Services.AddSingleton(new BlobServiceClient(blobConnectionString));
+    if (string.IsNullOrWhiteSpace(blobConnectionString))
+    {
+        throw new InvalidOperationException(
+            "Blob storage connection string not configured. Set BlobStorage:ConnectionString, AzureWebJobsStorage, BlobStorageConnectionString, or ConnectionStrings:BlobStorage.");
+    }
+
+    return new BlobServiceClient(blobConnectionString);
+});
 
 // Application Insights isn't enabled by default. See https://aka.ms/AAt8mw4.
 // builder.Services
@@ -25,3 +38,8 @@ builder.Services.AddSingleton(new BlobServiceClient(blobConnectionString));
 //     .ConfigureFunctionsApplicationInsights();
 
 builder.Build().Run();
+
+internal sealed class BlobStorageOptions
+{
+    public string? ConnectionString { get; set; }
+}
